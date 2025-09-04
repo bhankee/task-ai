d4910b9f01071e9e5a4cc13e403963cfe07d409d
bradhankee <brad.hankee@gmail.com>
2025-09-04 17:10:18 -0500

    Added agent history to preserve context

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from pydantic_core.core_schema import model_field
 from langchain.tools import tool
 from langchain.agents import create_openai_tools_agent, AgentExecutor
+from todoist_api_python.api import TodoistAPI
 
 load_dotenv()
 
 todoist_api_key = os.getenv("TODOIST_API_KEY")
 gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+todoist = TodoistAPI(todoist_api_key)
 
 
 
@@ -24,31 +27,41 @@ llm = ChatGoogleGenerativeAI(
 )
 
 system_prompt = "You are a helpful assistant. You will help the user add tasks"
-user_input = "add task to buy some milk from the local shop"
+
 
 @tool
-def add_task():
-    """" Add a task to the users task list """
-    print("Adding task")
-
-    print("Task added")
+def add_task(task, desc=None):
+    """" Add a task to the users task list. Use this when the user wants to add or create a task """
+    todoist.add_task(content=task, description=desc)
 
 tools = [add_task]
 
 
 
 prompt = ChatPromptTemplate([("system", system_prompt),
-                             ("user", user_input),
+                             MessagesPlaceholder("history"),
+                             ("user", "{input}"),
                              MessagesPlaceholder("agent_scratchpad")
                              ])
 
 # chain = prompt | llm | StrOutputParser()
 
 agent = create_openai_tools_agent(llm, tools, prompt)
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=False)
 
 
-response = agent_executor.invoke({"input": user_input})
 
-print(response)
+
+
+history= []
+while True:
+    user_input = input("You: ")
+    response = agent_executor.invoke({"input": user_input, "history":history})
+
+    print(response['output'])
+
+    # Add context history so the discussion can build upon previous questions or statements
+    history.append(HumanMessage(content=user_input))
+    history.append(AIMessage(content=response['output']))
+
 
