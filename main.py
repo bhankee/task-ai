f71c112f15c2f4c5c99ca5e6e9281b77e0bfba0a
bradhankee <brad.hankee@gmail.com>
2025-09-04 14:46:28 -0500

    Added main functions from tools

---
+++ main.py
@@ -0,0 +1,54 @@
+from dotenv import load_dotenv
+import os
+
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_google_genai import ChatGoogleGenerativeAI
+from pydantic_core.core_schema import model_field
+from langchain.tools import tool
+from langchain.agents import create_openai_tools_agent, AgentExecutor
+
+load_dotenv()
+
+todoist_api_key = os.getenv("TODOIST_API_KEY")
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+
+
+
+llm = ChatGoogleGenerativeAI(
+    model='gemini-2.5-flash',
+    google_api_key=gemini_api_key,
+    temperature=0.3
+)
+
+system_prompt = "You are a helpful assistant. You will help the user add tasks"
+user_input = "add task to buy some milk from the local shop"
+
+@tool
+def add_task():
+    """" Add a task to the users task list """
+    print("Adding task")
+
+    print("Task added")
+
+tools = [add_task]
+
+
+
+prompt = ChatPromptTemplate([("system", system_prompt),
+                             ("user", user_input),
+                             MessagesPlaceholder("agent_scratchpad")
+                             ])
+
+# chain = prompt | llm | StrOutputParser()
+
+agent = create_openai_tools_agent(llm, tools, prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+
+response = agent_executor.invoke({"input": user_input})
+
+print(response)
+
